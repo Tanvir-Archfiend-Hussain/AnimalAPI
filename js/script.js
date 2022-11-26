3185449d28d3a8474151419fc4967e1a2e13b021
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-26 17:34:10 +0000

    Script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const cat_btn = document.getElementById("cat_btn");
+const dog_btn = document.getElementById("dog_btn");
+const cat_result = document.getElementById("cat_result");
+const dog_result = document.getElementById("dog_result");
